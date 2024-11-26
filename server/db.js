c6713c7557098a2cc199b56507778054878975ae
Gargi Chandrakar <74736711+gargi2718@users.noreply.github.com>
2024-11-26 18:48:40 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,8 +4,8 @@ const dotenv = require ('dotenv');
 dotenv.config();
 
 const DBConnection = async () => {
-    
-    const MONGO_URI = process.env.MONGODB_URL;
+    const MONGO_URI = "mongodb+srv://gargi2001ee89:12345@cluster0.u733clp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+   // const MONGO_URI = process.env.MONGODB_URL;
     try {
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
